9d1f9efe6d6ddd23da6a714fba980a8c87b30f94
vladgepar <vladgepar@gmail.com>
2024-06-13 20:13:40 -0300

    Update API
    Commit para o Update da API

---
+++ API/API/Controllers/LivrosController.cs
@@ -0,0 +1,63 @@
+using Core.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LivrosController : ControllerBase
+    {
+        private readonly ILivroService _livroService;
+
+        public LivrosController(ILivroService livroService)
+        {
+            _livroService = livroService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var livros = await _livroService.GetAllAsync();
+            return Ok(livros);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var livro = await _livroService.GetByIdAsync(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return Ok(livro);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Livro livro)
+        {
+            await _livroService.AddAsync(livro);
+            return CreatedAtAction(nameof(GetById), new { id = livro.Id }, livro);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return BadRequest();
+            }
+            await _livroService.UpdateAsync(livro);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _livroService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API/API/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Core.Interfaces;
+using Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicos
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 21))));
+
+// Dependencias
+builder.Services.AddScoped<ILivro, LivroRepository>();
+builder.Services.AddScoped<ILivroService, LivroService>();
+
+var app = builder.Build();
+
+// Http request
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
